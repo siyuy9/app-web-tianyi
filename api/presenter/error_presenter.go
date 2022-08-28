e2ddf3ea7025b3674e13134d8793d3c1329c7d9d
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2022-08-28 20:41:39 +0300

    docker

--- api/presenter/error_presenter.go
+++ api/presenter/error_presenter.go
@@ -1,10 +1,13 @@
 package presenter
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	pkgError "gitlab.com/kongrentian-group/tianyi/v1/pkg/error"
 )
 
 func Error(context *fiber.Ctx, err pkgError.Error) error {
+	log.Println("ERROR", err)
 	return context.Status(err.StatusCode()).JSON(&ResponseError{Error: err})
 }
